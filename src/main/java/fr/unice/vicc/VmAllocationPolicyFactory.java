58917f82b9096ec5c4594f064ce19c4cb01beab8
walidmoussa <walid.moussa.a@gmail.com>
2014-12-21 19:39:47 +0100

    Update VmAllocationPolicyFactory.java

--- src/main/java/fr/unice/vicc/VmAllocationPolicyFactory.java
+++ src/main/java/fr/unice/vicc/VmAllocationPolicyFactory.java
@@ -9,10 +9,12 @@ import java.util.List;
  * @author Fabien Hermenier
  */
 public class VmAllocationPolicyFactory {
-
+// c'est la methode dans laquelle on cree des policy en utilisant un ensemble de hosts
     VmAllocationPolicy make(String id, List<PowerHost> hosts) {
         switch (id) {
-            case "naive": /* return new NaiveVmAllocationPolicy(hosts)*/
+            case "naive":  return new NaiveVmAllocationPolicy(hosts);
+            case "antiAffinity": return new AntiAffinityVMAllocationPolicy(hosts);
+            case "balance": return new BalanceVmAllocationPolicy(hosts);
         }
         throw new IllegalArgumentException("No such policy '" + id + "'");
     }
